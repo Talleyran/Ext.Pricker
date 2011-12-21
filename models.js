058193deb1069df3a3c3a78a01685214544199e7
Kirill Jakovlev <special-k@li.ru>
2011-12-22 02:21:09 +0600

    stores configuration

---
+++ models.js
@@ -0,0 +1,4 @@
+FieldStore = Ext4.create('Ext.data.Store', { fields: ['id', 'name'] })
+ChartStore = Ext4.create('Ext.data.JsonStore', { fields: ['name', 'data1', 'data2', 'data3', 'data4', 'data5', 'data6', 'data7', 'data8', 'data9'] })
+TypeStore = Ext4.create('Ext.data.Store', { fields: ['id', 'name'] })
+
