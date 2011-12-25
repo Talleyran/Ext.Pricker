5c51b880b4c99bdf1f39e8aea25969669d3aeb06
Kirill Jakovlev <special-k@li.ru>
2011-12-26 02:52:53 +0600

    stores init file

--- stores_init.js
+++ stores_init.js
@@ -1,5 +1,4 @@
 window.FieldStoreX = Ext4.create('Ext.data.Store', { fields: ['id', 'name'] })
 window.FieldStoreY = Ext4.create('Ext.data.Store', { fields: ['id', 'name'] })
-window.ChartStore = Ext4.create('Ext.data.JsonStore', { fields: [] } )
 window.TypeStore = Ext4.create('Ext.data.Store', { fields: ['id', 'name'] })
-
+window.ChartStore = Ext4.create('Ext.data.JsonStore')
