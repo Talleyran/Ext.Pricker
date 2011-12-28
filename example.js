76578b24adec614f4bb6eac8b5ba4394ae84d72c
Kirill Jakovlev <special-k@li.ru>
2011-12-28 23:21:20 +0600

    example

--- example.js
+++ example.js
@@ -11,15 +11,25 @@ Ext4.onReady(function() {
     //var l1 = new OpenLayers.Layer.WMS("t1", "http://wms.latlon.org", {layers: "yhsat"})
     //var l2 = new OpenLayers.Layer.WMS("t2", "http://wms.latlon.org", {layers: "yasat"}, {isBaseLayer: false})
     var l1 = new OpenLayers.Layer.WMS("t1", "http://oceanviewer.ru/eko/wms", {layers: "eko_merge"})
-    var l2 = new OpenLayers.Layer.WMS("t2", "http://oceanviewer.ru/resources/ru_hydrometcentre_46/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_2"}, {isBaseLayer: false})
+    var l2 = new OpenLayers.Layer.WMS("t2", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_1"}, {isBaseLayer: false})
+    var l3 = new OpenLayers.Layer.WMS("t3", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_2"}, {isBaseLayer: false})
+    var l4 = new OpenLayers.Layer.WMS("t4", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_3"}, {isBaseLayer: false})
+    var l5 = new OpenLayers.Layer.WMS("t5", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_4"}, {isBaseLayer: false})
+    var l6 = new OpenLayers.Layer.WMS("t6", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_5"}, {isBaseLayer: false})
+    var l7 = new OpenLayers.Layer.WMS("t7", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_6"}, {isBaseLayer: false})
+    var l8 = new OpenLayers.Layer.WMS("t8", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_7"}, {isBaseLayer: false})
+    var l9 = new OpenLayers.Layer.WMS("t9", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_8"}, {isBaseLayer: false})
+    var l10 = new OpenLayers.Layer.WMS("t10", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_9"}, {isBaseLayer: false})
+    var l11 = new OpenLayers.Layer.WMS("t11", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_10"}, {isBaseLayer: false})
+    var l12 = new OpenLayers.Layer.WMS("t12", "http://oceanviewer.ru/resources/wms", {layers: "ru_hydrometcentre_46:ru_hydrometcentre_46_11"}, {isBaseLayer: false})
 
-    map.addLayers([l1 , l2 ])
+    map.addLayers([l1 , l2 /* , l3 , l4 , l5 , l6 , l7 , l8 , l9 , l10 , l11 */ ])
 
     map.addControl(new OpenLayers.Control.LayerSwitcher())
 
     var pricker = new GeoExt.Pricker({
          map: map
-        ,layers: [l2] //adding layers
+        ,layers: [ l2 , l3 , l4 , l5 /*, l6 , l7 , l8 , l9 , l10 , l11*/ ] //adding layers
         ,aliaseUrl: '/OceanViewer2/translate'
         ,getInfoUrl: '/resources/wms'
         ,nameTitleAlias: 'назв.слоя'
