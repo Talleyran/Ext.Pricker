2e03d14f2e59ab1292a9831e70fc6b5212d59b39
Kirill Jakovlev <special-k@li.ru>
2012-06-06 19:35:57 +0400

    locale

--- javascripts/locale/ru.js
+++ javascripts/locale/ru.js
@@ -1,21 +1,22 @@
-GeoExt.Pricker.prototype.lineType = 'Линейный'
-GeoExt.Pricker.prototype.columnType = 'Столбчатый'
-GeoExt.Pricker.prototype.areaType = 'Площадной'
+GeoExt.Pricker.prototype.lineType = 'Линейный';
+GeoExt.Pricker.prototype.columnType = 'Столбчатый';
+GeoExt.Pricker.prototype.areaType = 'Площадной';
 
-GeoExt.PrickerParser.prototype.nameTitleAlias = 'Слои'
+GeoExt.PrickerParser.prototype.nameTitleAlias = 'Слои';
 
-GeoExt.PrickerWindow.prototype.title = 'Построить график'
-GeoExt.PrickerWindow.prototype.fieldComboName1 = 'Ось Х'
-GeoExt.PrickerWindow.prototype.fieldComboName2 = 'Ось Y'
-GeoExt.PrickerWindow.prototype.defaultAxisTitle1 = 'X'
-GeoExt.PrickerWindow.prototype.defaultAxisTitle2 = 'Y'
-GeoExt.PrickerWindow.prototype.typeComboName = 'Тип графика'
-GeoExt.PrickerWindow.prototype.addText = 'Добавить'
-GeoExt.PrickerWindow.prototype.deleteText = 'Удалить'
-GeoExt.PrickerWindow.prototype.saveText = 'Сохранить'
-GeoExt.PrickerWindow.prototype.addLayerWinTitle = 'Добавление слоя'
-GeoExt.PrickerWindow.prototype.canselText = 'Отмена'
-GeoExt.PrickerWindow.prototype.okText = 'Ok'
+GeoExt.PrickerWindow.prototype.title = 'Построить график';
+GeoExt.PrickerWindow.prototype.fieldComboName1 = 'Ось Х';
+GeoExt.PrickerWindow.prototype.fieldComboName2 = 'Ось Y';
+GeoExt.PrickerWindow.prototype.defaultAxisTitle1 = 'X';
+GeoExt.PrickerWindow.prototype.defaultAxisTitle2 = 'Y';
+GeoExt.PrickerWindow.prototype.typeComboName = 'Тип графика';
+GeoExt.PrickerWindow.prototype.addText = 'Добавить';
+GeoExt.PrickerWindow.prototype.deleteText = 'Удалить';
+GeoExt.PrickerWindow.prototype.saveText = 'Сохранить';
+GeoExt.PrickerWindow.prototype.addLayerWinTitle = 'Добавление слоя';
+GeoExt.PrickerWindow.prototype.canselText = 'Отмена';
+GeoExt.PrickerWindow.prototype.okText = 'Ok';
+GeoExt.PrickerWindow.prototype.layerName = 'Слои';
 
-gxp.plugins.PrickerTool.prototype.tooltip = 'Графики'
-gxp.plugins.PrickerTool.prototype.menuText = 'Графики'
+gxp.plugins.PrickerTool.prototype.tooltip = 'Графики';
+gxp.plugins.PrickerTool.prototype.menuText = 'Графики';
